c5c9781ec089cab0afad55e722b78337b7a6772a
i3mDevep <ingmichaelheano10@gmail.com>
2022-10-02 13:46:36 -0500

    create react app with vite

---
+++ thumbnail-generator-ui/vite.config.ts
@@ -0,0 +1,30 @@
+/* eslint-disable quotes */
+/* eslint-disable import/no-extraneous-dependencies */
+import path from "path";
+import { Alias, defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+import * as tsconfig from "./tsconfig.paths.json";
+
+function readAliasFromTsConfig(): Alias[] {
+  const pathReplaceRegex = new RegExp(/\/\*$/, "");
+  return Object.entries(tsconfig.compilerOptions.paths).reduce(
+    (aliases, [fromPaths, toPaths]) => {
+      const find = fromPaths.replace(pathReplaceRegex, "");
+      const toPath = toPaths[0].replace(pathReplaceRegex, "");
+      const replacement = path.resolve(__dirname, toPath);
+      aliases.push({ find, replacement });
+      return aliases;
+    },
+    [] as Alias[]
+  );
+}
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": "https://your-api-server.com",
+    },
+  },
+});
